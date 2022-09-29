3e8bd2b0d59ab2b32d0043924552c6a333b97841
jOyEjiN2 <angel000604@naver.com>
2022-09-29 13:11:41 +0900

    CBV로 포스트 목록/상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ #IP주소/blog/
-    path('',views.index), #IP주소/blog/ , 목록 페이지?  (추가하려면 뒤에 꼭 , 넣어야함)
-    path('<int:pk>/',views.single_post_page)  #int 숫자가 옴
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view())
+    #path('',views.index), #IP주소/blog/ , 목록 페이지?  (추가하려면 뒤에 꼭 , 넣어야함)
+    #path('<int:pk>/',views.single_post_page)  #int 숫자가 옴
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
-def index(request):      # all => 다가져오겠다
-    posts = Post.objects.all().order_by('-pk') #pk=primary key /-pk 거꾸로 정렬(최신 게시글 순서대로)
-    return render(request, 'blog/index.html',{'posts':posts})
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # 템플릿은 모델명_list.html => post_list.html
+    #blog.html을 post_list.html로 바꿔줌
 
-def single_post_page(request,pk):
-    post = Post.objects.get(pk=pk)
+class PostDetail(DetailView):
+    model = Post
+    #템플릿은 모델명_detail.html => post_detail.html
+    #single_post_page.html을 post_detail.html로 바꿔줌
+
+
+
+#def index(request):      # all => 다가져오겠다
+#    posts = Post.objects.all().order_by('-pk') #pk=primary key /-pk 거꾸로 정렬(최신 게시글 순서대로)
+#    return render(request, 'blog/index.html',{'posts':posts})
+
+#def single_post_page(request,pk):
+#    post = Post.objects.get(pk=pk)
                         #get => 특정한 값을 가지고 오겠다
-    return render(request, 'blog/single_post_page.html', {'post':post})
+#    return render(request, 'blog/single_post_page.html', {'post':post})
