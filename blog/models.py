84f77125b58b8c9a7f2ca4267f5d25f1147a7c88
jOyEjiN2 <angel000604@naver.com>
2022-10-11 16:10:46 +0900

    포스트 목록/상세 페이지에 첨부파일, 미리보기, 필터 적용하기

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+import os
 
 # Create your models here.
-# model이 변경되면 migrations을 해야한다?
+# model이 변경되면 migrations을 해야한다
+#함수 추가할 경우에는 migration할 필요 없음 , Field가 추가될 때는 migration 해줘야함
+
 class Post(models.Model): # post라고 하는 이름의 테이블을 만들겠다고 선언
-    title = models.CharField(max_length=30)        # title이라고 하는 필드가 만들어짐, charfield 문자열, 최대 30글자까지
+    title = models.CharField(max_length=30)        # title이라고 하는 필드가 만들어짐, charfield 문자열 글자 수 제한, 최대 30글자까지
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
@@ -18,9 +22,17 @@ class Post(models.Model): # post라고 하는 이름의 테이블을 만들겠�
 
     # author 나중에 추가
 
+
     def __str__(self):
         return f'[{self.pk}]{self.title}  {self.created_at}' # 화면에 어떻게 출력되는지
                    # pk = primary key   타이틀   시간          순으로 포스트 제목 출력
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):    #함수 선언
+        return os.path.basename(self.file_upload.name) #base이름은 빼고 파일 이름만 남게 함
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1] #a.text => a   text (.을 기준으로 나뉘어진다)
+                                            # 제일 마지막에 해당되는 배열번호
