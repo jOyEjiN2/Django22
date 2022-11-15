4ffad8c4a73a44cae3ec4f0cb9f377001fd676df
jOyEjiN2 <angel000604@naver.com>
2022-11-15 15:15:13 +0900

    태그 선택란 추가하기

--- blog/views.py
+++ blog/views.py
@@ -4,27 +4,52 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 #로그인 사용자가 글을 입력할 수 있도록
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 # Create your views here.
 
 class PostUpdate(LoginRequiredMixin,UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tag']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category'] #, 'tag'
     template_name = 'blog/post_update_form.html'
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
             return super(PostUpdate, self).dispatch(request, *args, *kwargs)
         else:
             raise PermissionDenied
+
+    def form_valid(self,form):
+        response = super(PostUpdate,self).form_valid(form)
+        self.object.tags.clear()
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(',', ';')
+            tag_list = tags_str.split(';')
+            for t in tag_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+        return response
+
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostUpdate,self).get_context_data()
+        if self.object.tags.exists:
+            tag_str_list = list()
+            for t in self.object.tag.all():
+                tag_str_list.append(t.name)
+            context['tags_str_default'] = ';'.join(tag_str_list)
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count
         return context
 
 class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):  #CreateView,LoginRequiredMixin들을 상속받는다
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category'] # ,'tags'
     #모델명_form.html => 템플릿이 자동으로 호출
 
     def test_func(self):
@@ -36,7 +61,20 @@ class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):  #CreateVie
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_superuser or current_user.is_staff):  #true면 로그인된 인증된 유저인 것
             form.instance.author = current_user
-            return super(PostCreate,self).form_valid(form)
+            response = super(PostCreate,self).form_valid(form)
+            tags_str = self.request.POST.get('tags_str')
+            if tags_str:
+                tags_str = tags_str.strip()
+                tags_str = tags_str.replace(',',';')
+                tag_list = tags_str.split(';')
+                for t in tag_list :
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+            return response
         else:
             return redirect('/blog/')
     def get_context_data(self, *, object_list=None, **kwargs):
